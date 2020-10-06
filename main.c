a00e7230c9ae625f856c45e7890770376b71ba13
Thobiz <69186131+Thobiz@users.noreply.github.com>
2020-10-06 13:12:12 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+    Program:while,Do while
+    Authority:Mr Jirayut
+    Date:05/06/2020
+*/
+#include<stdio.h>
+int main(){
+    printf("Main System\n");
+   // while1();
+   // int for1();
+    int dowhile1();
+    int dowhile2();
+}
